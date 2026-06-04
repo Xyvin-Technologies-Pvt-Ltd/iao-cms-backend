d83520653b5bec785e435a94f6a71d335ed07c76
Sarath Chandran <sarathc.dev@gmail.com>
2026-06-04 10:04:28 +0530

    feat(cms): add new boolean attributes for postacademic module visibility
    - Introduced 'show_on_postacademic_module' and 'show_on_postacademic_programmes' boolean attributes to the pam-module schema, enhancing visibility control for postacademic content.
    - Updated type definitions to include the new attributes, ensuring consistency and localization support across the schema.

---
+++ src/api/programmes-postacademic-programme/controllers/programmes-postacademic-programme.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * programmes-postacademic-programme controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::programmes-postacademic-programme.programmes-postacademic-programme');
---
+++ src/api/programmes-postacademic-programme/routes/programmes-postacademic-programme.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * programmes-postacademic-programme router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::programmes-postacademic-programme.programmes-postacademic-programme');
